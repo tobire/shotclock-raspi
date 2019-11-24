dbb7d85b8dc9b6cc128ce92028c2067afad016ee
tobire <tobire@users.noreply.github.com>
2019-11-24 19:10:55 +0100

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,10 +1,10 @@
 PORT=8060
 RETRY_WAIT=5
 
-OWN_IP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
-NETWORK="${OWN_IP%.*}.0"
-
 while true; do
+	OWN_IP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+	NETWORK="${OWN_IP%.*}.0"
+
 	echo "Scanning for servers in network $NETWORK on port $PORT..."
 	nmap -p $PORT $NETWORK/24 -oG scan.out > /dev/null
 	sed -ni '/open/p' scan.out
